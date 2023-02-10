7845e1f0f6559bd14c936dedaa4510c62a289ae7
Jingru Xu <jingruxu@Jingrus-Air.lan>
2023-02-10 14:46:53 -0500

    built Header component

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { getAuth, signOut } from "firebase/auth";
 
+import Header from "../components/Header";
 export interface IHomeProps {}
 
 const Home: React.FC<IHomeProps> = (props) => {
@@ -10,7 +11,20 @@ const Home: React.FC<IHomeProps> = (props) => {
 
   return (
     <div>
-      <p>Home Page (Protected by Firebase!)</p>
+      <Header />
+      <form action="">
+        <h2>Top Priorities</h2>
+      </form>
+      <form action="">
+        <h2>Reminders</h2>
+      </form>
+      <form action="">
+        <h2>To Do</h2>
+      </form>
+      <form action="">
+        <h2>Notes</h2>
+      </form>
+
       <button
         onClick={() => {
           signOut(auth);
